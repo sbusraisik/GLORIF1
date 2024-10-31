952db3b318e23bc3b71ab0b583be489f9651c7d4
sbusraisik <129958550+sbusraisik@users.noreply.github.com>
2024-10-31 20:59:01 +0100

    Update 2_RF_1_tune.R

--- Code/2_RandomForest/2_RF_1_tune.R
+++ Code/2_RandomForest/2_RF_1_tune.R
@@ -2,10 +2,10 @@
 source('/home/bisik/Practical/R/fun_0_loadLibrary.R')
 ####-------------------------------####
 
-outputDir <- '/scratch/bisik/Practical_NEW/RF/tune/'
+outputDir <- '/scratch-shared/bisik/Data/tune/'
 dir.create(outputDir, showWarnings = F, recursive = T)
 
-train_data <- vroom('/home/bisik/Practical/bigTable_allpredictors_final_2.csv', show_col_type=F)
+train_data <- vroom('/scratch-shared/bisik/Data/bigTable_allpredictors_filtered_95.csv', show_col_type=F)
 rf_input <- train_data %>% select(., -grdc_no, -cell_no_land, -datetime)
 
 
@@ -15,8 +15,8 @@ num_predictors <- ncol(rf_input) - 1  # Subtract 1 to exclude the target variabl
 #--------------RF---------------
 #-----------1. Tune parameter---------------
 hyper_grid <- expand.grid(
-  ntrees = c(10,100,300,500,900), 
-  #ntrees = 200, #only use 200 trees for rapid tuning
+  #ntrees = c(10,100,300,500,900), 
+  ntrees = 200, #only use 200 trees for rapid tuning
 #~   mtry = c(27)
   mtry = 1:num_predictors  # Ensure mtry is within the range of number of predictors
 )
@@ -31,5 +31,5 @@ for(i in 1:nrow(hyper_grid)){
   hyper_grid$OOB_RMSE[i] <- sqrt(hyper_trains[[i]]$prediction.error)
 }
   
-print(paste0('output csv file: ', outputDir, 'hyper_grid_mtry_5-30.csv'))
+print(paste0('output csv file: ', outputDir, 'hyper_grid_mtry_10-900.csv'))
 write.csv(hyper_grid, paste0(outputDir, 'hyper_grid_ntrees_10-900.csv'), row.names = F)

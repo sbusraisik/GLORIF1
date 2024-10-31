8897e82977b0c6c2d5c31d3d4be0b04921c12eaa
sbusraisik <129958550+sbusraisik@users.noreply.github.com>
2024-10-31 18:56:09 +0100

    Update and rename gsim_without_grdc.py to 1st_action_gsim_without_grdc.py

--- Code/0_preprocess_gsim/1st_action_gsim_without_grdc.py
+++ Code/0_preprocess_gsim/1st_action_gsim_without_grdc.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 # Load the CSV files into DataFrames
-gsim_df = pd.read_csv('/home/bisik/Practical/GSIM_metadata/GSIM_catalog/GSIM_catchment_characteristics.csv')
+gsim_df = pd.read_csv('/scratch-shared/bisik/Data/preprocess/preproces_gsim/GSIM_catchment_characteristics.csv')
 #gsim_df = pd.read_csv('/home/bisik/Practical/gsim_preprocess/station_to_pixel_mapping_gsim.csv')
-grdc_df = pd.read_csv('/home/bisik/Practical/stationLatLon_grdc.csv')
+grdc_df = pd.read_csv('/scratch-shared/bisik/Data/preprocess/preproces_grdc/stationLatLon_grdc.csv')
 
 # Rename the columns in gsim_df
 gsim_df.rename(columns={'lat.org': 'lat', 'long.org': 'lon'}, inplace=True)
@@ -18,4 +18,4 @@ matching_indices = gsim_df[gsim_df.set_index(['lat', 'lon']).index.isin(merged_d
 filtered_gsim_df = gsim_df.drop(matching_indices)
 
 # Save the filtered DataFrame to a new CSV file
-filtered_gsim_df.to_csv('/home/bisik/Practical/gsim_preprocess/filtered_GSIM_catchment_characteristics.csv', index=False)
+filtered_gsim_df.to_csv('/scratch-shared/bisik/Data/preprocess/preproces_gsim/filtered_GSIM_catchment_characteristics.csv', index=False)
